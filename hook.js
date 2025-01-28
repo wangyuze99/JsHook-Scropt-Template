2001dbe14c37e1b72804b0d3c7dc08bbd2dd484e
wangyuze99 <97773316+wangyuze99@users.noreply.github.com>
2025-01-29 01:53:34 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -7,8 +7,8 @@ export const hook = () => {
     logger.toast('start hook');
     Il2Cpp.perform(() => {
         logger.toast('unity Version: ', Il2Cpp.unityVersion);
-
-game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("update105Log").implementation = function () { };
+        function prevenlog() {
+        game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("update105Log").implementation = function () { };
 
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateLog").implementation = function () { };
 
@@ -21,28 +21,27 @@ game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("update105Log").impl
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateItemLIstLog").implementation = function () { };
 
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateMyData").implementation = function () { };
+        }
 
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("getAllChargeOrder").implementation = function () { };
 
-        
+exports.call = {
+     充值 :{
+         charge: {
         /** 受击扣血函数 */
         game.assembly().class('QianYi.Game.Hero.V_Charge_Sys_UI').method("chargeByOther105").implementation = function (charge_val, func_, sub_func_) {
-    if (Mod.var.slider3) {
-        // 使用滑动条的值
-        charge_val = Mod.var.slider1;
-        func_ = Mod.var.slider2;
-        sub_func_ = Mod.var.slider3;
-    }
-    
-    // 调用原方法
-    this.method("chargeByOther105").invoke(charge_val, func_, sub_func_);
-};
-        /* 怪自杀 */
+            this.method("chargeByOther105").invoke(charge_val, func_, sub_func_)
+            if (chage_val == 6){
+            logger.toast("执行失败")
+            }
+            }
+        },
+  /*      
         game.assembly().class('GamePlay.PlayerObj').method("UpdateLogic").implementation = function (dt) {
             this.method("UpdateLogic").invoke(dt);
             if (Mod.var.switch2 && this.field('GroupID').value != 99 && this.method("get_IsMonster").invoke()) {
                 this.method("OnHPZero").invoke(true, true, this);
             }
-        };
-    });
+        };*/
+    },
 };
