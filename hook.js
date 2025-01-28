afa96ff5d19b91bf1fd0133370c8af12afb9383c
wangyuze99 <97773316+wangyuze99@users.noreply.github.com>
2025-01-29 02:04:43 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -7,41 +7,26 @@ export const hook = () => {
     logger.toast('start hook');
     Il2Cpp.perform(() => {
         logger.toast('unity Version: ', Il2Cpp.unityVersion);
-        
+
+        // 钩子方法，重写为空函数
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("update105Log").implementation = function () { };
-
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateLog").implementation = function () { };
-
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateCoinLog").implementation = function () { };
-
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateGoldLog").implementation = function () { };
-
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateItemLog").implementation = function () { };
-
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateItemLIstLog").implementation = function () { };
-
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("updateMyData").implementation = function () { };
-        
-
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("getAllChargeOrder").implementation = function () { };
 
-exports.call = {
-     充值 :{
-         charge: {
-        /** 受击扣血函数 */
-        game.assembly().class('QianYi.Game.Hero.V_Charge_Sys_UI').method("chargeByOther105").implementation = function (charge_val, func_, sub_func_) {
-            this.method("chargeByOther105").invoke(charge_val, func_, sub_func_)
-            //if (chage_val == 6){
-            //logger.toast("执行失败")
-         //   }
+        exports.call = {
+            充值: {
+                charge: {
+                    // 重写 "chargeByOther105" 方法
+                    chargeByOther105: function (charge_val, func_, sub_func_) {
+                        this.method("chargeByOther105").invoke(charge_val, func_, sub_func_);
+                    }
+                }
             }
-        },
-  /*      
-        game.assembly().class('GamePlay.PlayerObj').method("UpdateLogic").implementation = function (dt) {
-            this.method("UpdateLogic").invoke(dt);
-            if (Mod.var.switch2 && this.field('GroupID').value != 99 && this.method("get_IsMonster").invoke()) {
-                this.method("OnHPZero").invoke(true, true, this);
-            }
-        };*/
-},
+        };
+    });
 };
