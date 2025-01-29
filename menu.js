6b51fd22bbef1a1032c98d79c20ba71840e2f1de
wangyuze99 <97773316+wangyuze99@users.noreply.github.com>
2025-01-29 17:11:57 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -36,8 +36,8 @@ export const getMenu = () => {
                             val: "6",
                             callback: (data) => {
                                 Il2Cpp.perform(() => {
-                                    game.assembly().class('QianYi.Game.Hero.V_Charge_Sys_UI').method("chargeByOhter105").implementation = function(charge_val, func_, sub_func_) {
-                                        this.method("chargeByOhter105").invoke(data.val, 2, sub_func_);
+                                    game.assembly().class('QianYi.Game.Hero.V_Charge_Sys_UI').method("chargeByOther105").implementation = function(charge_val, func_, sub_func_) {
+                                        this.method("chargeByOther105").invoke(data.val, 2, sub_func_);
                                     };
                                 });
                             }
