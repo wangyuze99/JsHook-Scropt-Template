2bf8bfd6a7e6987dbe590c905aad7b223b73d438
wangyuze99 <97773316+wangyuze99@users.noreply.github.com>
2025-01-29 14:30:56 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 import 'frida-il2cpp-bridge';
 import { Mod } from './mod';
-import  hook_1 from './hook';
+import hook from './hook';
 
 /**
  * 通过 id 查找元素
@@ -8,9 +8,9 @@ import  hook_1 from './hook';
  * @returns {MenuItem}
  */
 function findById(id) {
-    return findItem((e) => e.id === id);
+    return findItem((e) => e.id == id);
 }
-export { findById };
+exports.findById = findById;
 
 /**
  * 通过 title 查找元素
@@ -18,9 +18,9 @@ export { findById };
  * @returns {MenuItem}
  */
 function findByTitle(title) {
-    return findItem((e) => e.title === title);
+    return findItem((e) => e.title == title);
 }
-export { findByTitle };
+exports.findByTitle = findByTitle;
 
 export const getMenu = () => {
     return [
@@ -31,77 +31,67 @@ export const getMenu = () => {
                     title: '充值',
                     item: [
                         {
-                            type: 'input',
-                            title: '金额',
-                            val: '6',
-                            callback: e => {
-               
-                    hook_1.call.充值.charge(e.val, 2, 1);
-                  
-              
-                            },
-                        },
-                    
-                        /*{
-                            type: 'button',
-                            title: '充值',
+                            type: "input",
+                            title: "金额",
+                            val: "6",
                             callback: (data) => {
-                                hook.call.充值.charge(parseInt(data.val));
-                            },
-                        },*/
+                                // Call the hooked chargeByOhter105 method with custom parameters
+                                hook.call.充值.charge(parseInt(data.val), 2, 1);
+                            }
+                        },
                         {
-                            type: 'switch',
-                            title: '无敌',
+                            type: "switch",
+                            title: "无敌",
                             val: Mod.var.switch1,
                             callback: (data) => {
                                 Mod.var.switch1 = data.val;
-                            },
+                            }
                         },
                         {
-                            type: 'switch',
-                            title: '自杀',
+                            type: "switch",
+                            title: "自杀",
                             val: Mod.var.switch2,
                             callback: (data) => {
                                 Mod.var.switch2 = data.val;
-                            },
-                        },
-                    ],
+                            }
+                        }
+                    ]
                 },
                 {
                     title: '关于',
                     item: [
                         {
                             type: 'text',
-                            val: 'Mod: ' + Mod.name,
+                            val: 'Mod: ' + Mod.name
                         },
                         {
                             type: 'text',
-                            val: 'Version: ' + Mod.version,
+                            val: 'Version: ' + Mod.version
                         },
                         {
                             type: 'text',
-                            val: 'Build: ' + Mod.build,
+                            val: 'Build: ' + Mod.build
                         },
                         {
                             type: 'text',
-                            val: 'CoreVersionCode: ' + runtime.coreVersionCode,
+                            val: 'CoreVersionCode: ' + runtime.coreVersionCode
                         },
                         {
                             type: 'button',
                             title: 'Dump unity',
                             callback: (data) => {
                                 Il2Cpp.perform(() => {
-                                    console.log('Unity Version:', Il2Cpp.unityVersion);
+                                    console.log(Il2Cpp.unityVersion);
                                     console.log('Dump Start');
                                     Il2Cpp.dump();
                                     console.log('Dump Complete');
                                 });
-                            },
-                        },
-                    ],
-                },
+                            }
+                        }
+                    ]
+                }
             ],
-            default: 1,
-        },
-    ];
+            default: 1
+        }
+    ]
 };
