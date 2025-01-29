e14a17f2c31ef45ed73cbe0c7297c9769485de4b
wangyuze99 <97773316+wangyuze99@users.noreply.github.com>
2025-01-29 12:59:17 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -42,7 +42,7 @@ export const getMenu = () => {
                   }
               
                             },
-                        }
+                        },
                     
                         /*{
                             type: 'button',
