e767bba407f56722da3d6730ed247972aa6f0e10
wangyuze99 <97773316+wangyuze99@users.noreply.github.com>
2025-01-30 21:01:46 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,4 +1,4 @@
-import Il2cpp from 'frida-il2cpp-bridge';
+import 'frida-il2cpp-bridge';
 import { Mod } from './mod';
 import logger from './logger';
 import game from './game';
@@ -18,24 +18,50 @@ export const hook = () => {
         game.assembly().class('QianYi.Game.Hero.MyHttpUtil').method("getAllChargeOrder").implementation = function () { };
 
         /** charge */
-        game.assembly().class('QianYi.Game.Hero.V_Charge_Sys_UI').method("chargeByOhter105").implementation = function (charge_val, func_, sub_func_) {
-    // 获取当前实例（非静态方法的 this 指向实例自身）
-    const instance = this.try();
+        import 'frida-il2cpp-bridge';
+import { Mod } from './mod';
+import logger from './logger';
+import game from './game';
 
-    if (!instance) {
-        logger.log('未找到 V_Charge_Sys_UI 实例');
-        return;
-    }
+export const hook = () => {
+    logger.toast('开始 Hook');
+    Il2Cpp.perform(() => {
+        logger.toast('Unity 版本: ' + Il2Cpp.unityVersion);
 
-    // 根据 Mod.var.switch3 修改参数
-    if (Mod.var.switch3) {
-        logger.toast('已启用 Charge 修改');
-        return instance.method("chargeByOhter105").invoke(6, 2, "test");
-    } else {
-        logger.toast('调用原方法');
-        return instance.method("chargeByOhter105").invoke(charge_val, func_, sub_func_);
-    }
-};
+        // 获取目标类和方法
+        const targetClass = Il2Cpp.Domain.assembly("Assembly-CSharp").image.class("QianYi.Game.Hero.V_Charge_Sys_UI");
+        const targetMethod = targetClass.method("chargeByOhter105");
+
+        // 检查方法是否存在
+        if (!targetMethod) {
+            logger.log('未找到 chargeByOhter105 方法');
+            return;
+        }
+
+        // 打印方法信息
+        logger.info('方法参数数量:', targetMethod.parameterCount);
+        logger.info('方法参数类型:', targetMethod.parameters.map(p => p.type.name).join(', '));
+        logger.info('方法返回类型:', targetMethod.returnType.name);
+
+        // Hook 方法
+        targetMethod.implementation = function (charge_val, func_, sub_func_) {
+            // 获取当前实例
+            const instance = this.try();
+            if (!instance) {
+                logger.info('未找到 V_Charge_Sys_UI 实例');
+                return;
+            }
+
+            // 根据 Mod.var.switch3 修改参数
+            if (Mod.var.switch3) {
+                logger.info('已启用 Charge 修改');
+                return instance.method("chargeByOhter105").invoke(6, 2, "test");
+            } else {
+                logger.info('调用原方法');
+                return instance.method("chargeByOhter105").invoke(charge_val, func_, sub_func_);
+            }
+        };
+
 
         /* 怪自杀 */
         game.assembly().class('QianYi.Game.Hero.MonsterObj').method("changeHp").implementation = function (val, damageType) {
